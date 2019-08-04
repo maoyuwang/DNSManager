e06d3ab485d443b9cb4eff6826eebc82e3667c47
Helen <wur7@rpi.edu>
2019-08-04 19:56:57 -0400

    hello

---
+++ src/Configuration.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Configuration extends JFrame {
+    
+
+    public Configuration() {
+       
+    }
+}
