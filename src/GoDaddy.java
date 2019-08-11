d250178c4c4ed9e720c541edf3534f5be302f5fc
maoyuwang <github@maoyu.wang>
2019-08-10 22:36:21 -0400

    Finish All getRecords Fake Data

--- src/GoDaddy.java
+++ src/GoDaddy.java
@@ -11,14 +11,14 @@ public class GoDaddy extends DNSProvider {
 
         Record[] r = new Record[8];
 
-        r[0] = new Record("myblog.me","A","www","114.114.114.114");
-        r[1] = new Record("myblog.me","A","test","23.123.122.31");
-        r[2] = new Record("myblog.me","A","myblog","23.14.53.12");
-        r[3] = new Record("myblog.me","CNAME","direct","qq.com");
-        r[4] = new Record("qqdie.com","A","www","21.234.123.4");
-        r[5] = new Record("qqdie.me","TEXT","vary1","TEST TEST Records");
-        r[6] = new Record("qqdie.com","CNAME","gg","www.qqdie.com");
-        r[7] = new Record("qqdie.com","AAAA","ipv6","2620:0:2820:2210:70a:77c2:d184:76b9");
+        r[0] = new Record("function.club","A","www","114.114.114.114");
+        r[1] = new Record("function.club","A","test","23.123.122.31");
+        r[2] = new Record("function.club","A","myblog","23.14.53.12");
+        r[3] = new Record("function.club","CNAME","direct","qq.com");
+        r[4] = new Record("function.blog","A","www","21.234.123.4");
+        r[5] = new Record("function.blog","TEXT","vary1","TEST TEST Records");
+        r[6] = new Record("function.blog","CNAME","gg","www.qqdie.com");
+        r[7] = new Record("function.blog","AAAA","ipv6","2620:0:2820:2210:70a:77c2:d184:76b9");
         return r;
     }
 
--- src/NameCom.java
+++ src/NameCom.java
@@ -9,16 +9,12 @@ public class NameCom extends DNSProvider {
     @Override
     public Record[] getRecords() {
 
-        Record[] r = new Record[8];
+        Record[] r = new Record[4];
 
-        r[0] = new Record("myblog.me","A","www","114.114.114.114");
-        r[1] = new Record("myblog.me","A","test","23.123.122.31");
-        r[2] = new Record("myblog.me","A","myblog","23.14.53.12");
-        r[3] = new Record("myblog.me","CNAME","direct","qq.com");
-        r[4] = new Record("qqdie.com","A","www","21.234.123.4");
-        r[5] = new Record("qqdie.me","TEXT","vary1","TEST TEST Records");
-        r[6] = new Record("qqdie.com","CNAME","gg","www.qqdie.com");
-        r[7] = new Record("qqdie.com","AAAA","ipv6","2620:0:2820:2210:70a:77c2:d184:76b9");
+        r[0] = new Record("v2ray.fun","A","www","12.3.2.331");
+        r[1] = new Record("v2ray.fun","TEXT","vary1","WEDASD-sadsaDWASD");
+        r[2] = new Record("v2ray.fun","CNAME","gg","www.goog.com");
+        r[3] = new Record("v2ray.fun","AAAA","ipv6","2300:0:2820:2110:70a:78c2:d184:76b9");
         return r;
     }
 
