3fe90b681c7b67d45762f7b1f6d7929b410d78e3
jasonh233 <jasonzht233@gamil.com>
2019-08-11 00:56:15 -0400

    update addRecord() void -> true

--- src/CloudFlare.java
+++ src/CloudFlare.java
@@ -5,8 +5,17 @@ import java.io.*;
 import okio.*;
 
 public class CloudFlare extends DNSProvider {
+    private HashMap<String, String> headers ;
     public CloudFlare(String pubKey, String privKey) {
         super(pubKey, privKey);
+        headers = new HashMap<String,String>();
+        headers.put("User-Agent","OkHttp Headers.java");
+        headers.put("X-Auth-Email",getPublicKey());
+        headers.put("X-Auth-Key",getPrivateKey());
+        headers.put("Content-Type","application/json");
+
+
+        //getRecords<-----TODO: zone_id - record_id
     }
 
     public HashMap<String,String> getZones()
@@ -15,13 +24,6 @@ public class CloudFlare extends DNSProvider {
 
         // 定义地址
         String url = "https://api.cloudflare.com/client/v4/zones?page=1&per_page=10&order=type&direction=asc";
-
-        // 设置 Headers
-        HashMap<String,String> headers = new HashMap<String,String>();
-        headers.put("User-Agent","OkHttp Headers.java");
-        headers.put("X-Auth-Email",getPublicKey());
-        headers.put("X-Auth-Key",getPrivateKey());
-        headers.put("Content-Type","application/json");
 
         // 调用得到结果
         String returnStr = API.GET(url,headers);
@@ -46,47 +48,77 @@ public class CloudFlare extends DNSProvider {
         return result;
     }
 
+    public JSONObject getDomainRecords(String domain) {
+        String url = String.format("https://api.cloudflare.com/client/v4/zones/%s/dns_records", this.getZones().get(domain) );
+        return JSON.parseObject(API.GET(url , headers));
+    }
+
+
     @Override
     public Record[] getRecords() {
 
-        //get all zones we have
+        //get all zones we have & Initialize
         HashMap<String,String> ZoneMap = this.getZones();
+        ArrayList<Record> RecordList = new ArrayList<Record>();
+        Record[] result = null ;
 
         //location-setting
         for( Map.Entry<String, String> entryitr : ZoneMap.entrySet() ) {
             String domain = entryitr.getKey();
-            String zone = entryitr.getValue();
 
-            String url = String.format("https://api.cloudflare.com/client/v4/zones/%s/dns_records", zone );
-
-            //settign headers
-            HashMap<String,String> headers = new HashMap<String,String>();
-            headers.put("User-Agent","OkHttp Headers.java");
-            headers.put("X-Auth-Email",getPublicKey());
-            headers.put("X-Auth-Key",getPrivateKey());
-            headers.put("Content-Type","application/json");
-
-            //get results
-            String returnStr = API.GET(url , headers);
-
-            //parse and get information form the json string
-            JSONObject jsonObj = JSON.parseObject(returnStr);
+            JSONObject jsonObj = this.getDomainRecords(domain);
             JSONArray records = jsonObj.getJSONArray("result");
 
-            for(int n=0 ; n < records.size() ; n++)
-            {
-                JSONObject record =records.getJSONObject(n);
+            for(int n=0 ; n < records.size() ; n++) {
+                JSONObject record = records.getJSONObject(n);
 
+                if (!record.get("type").toString().equals("MX")) {
+
+                    String RecordDomain = record.get("zone_name").toString();
+                    String RecordType = record.get("type").toString();
+                    String RecordName = record.get("name").toString();
+                    String RecordValue = record.get("content").toString();
+
+                    String[] name_lst = RecordName.split(".");
+                    if (name_lst.length != 2) {
+                        RecordName = name_lst[0];
+                    }
+
+                    RecordList.add(new Record(RecordDomain, RecordType, RecordName, RecordValue));
+                }
             }
-
         }
-
-
-        return null;
+        result = (Record[])RecordList.toArray();
+        return result;
     }
 
     @Override
     public boolean addRecord(Record r) {
+        //url
+        String url = String.format("https://api.cloudflare.com/client/v4/zones/%s/dns_records", this.getZones().get(r.domain) );
+
+        //json object- the details of new Record
+        JSONObject newrecord = new JSONObject();
+        newrecord.put("type" , r.type );
+        newrecord.put("name", r.name) ;
+        newrecord.put("content", r.value);
+        newrecord.put("ttl", 1);
+        newrecord.put("priority","10");
+        newrecord.put("proxied", false);
+
+        String returnStr = null ;
+        //post
+        try
+        {
+            returnStr = API.POST(url , headers, newrecord);
+        } catch(Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        //TODO: make returnStr -> JSONObject and check does it return success;
+        System.out.println(returnStr);
         return true;
     }
 
@@ -102,6 +134,10 @@ public class CloudFlare extends DNSProvider {
 
     public static void main(String[] args) {
         CloudFlare cf = new CloudFlare("wang.maoyu@hotmail.com","4c8394457166831f3d80fc7c98d9ad4a02ee1");
-        cf.getRecords();
+//        cf.getRecords();
+        cf.addRecord(new Record("ylws.me","A","test1", "114.114.114.114" ) );
+//        cf.updateRecord(new Reocrd("ylws.me","") )
     }
+
+
 }
