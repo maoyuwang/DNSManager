9aacecbe522a7206d08a3ccbf3f68e1d18d11f07
maoyuwang <github@maoyu.wang>
2019-08-15 19:23:32 -0400

    API Doc

--- src/API.java
+++ src/API.java
@@ -2,12 +2,19 @@ import com.squareup.okhttp.*;
 import java.util.HashMap;
 import com.alibaba.fastjson.*;
 
+/**
+ * A class that implements HTTP GET, POST, PUT, DELETE Methods.
+ */
 public class API {
+    /**
+     * Implementing HTTP GET Method.
+     * @param url   The url to make request.
+     * @param headersMap    A Map contains the headers to make the request.
+     * @return  The returned string after execute the GET Request.
+     */
     public static String GET(String url, HashMap<String,String> headersMap){
         Headers headers = Headers.of(headersMap);
         String resultStr = null;
-
-
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -28,6 +35,13 @@ public class API {
 
     }
 
+    /**
+     * Implementing HTTP POST Method.
+     * @param url   The URL to make post requests
+     * @param headersMap    The headers of the post request
+     * @param jsonObject    The parameters to this request
+     * @return  The result String after executing the POST request.
+     */
     public static String POST(String url, HashMap<String,String> headersMap, JSONObject jsonObject){
         Headers headers = Headers.of(headersMap);
         String resultStr = null;
